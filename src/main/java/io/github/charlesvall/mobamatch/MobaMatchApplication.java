f75c19caf5782ff51e1cd1c4bdaead7c48b3f102
CharlesVall <Charlessenardvallois@gmail.com>
2025-11-21 19:54:34 +0100

    init: spring boot mobamatch

---
+++ src/main/java/io/github/charlesvall/mobamatch/MobaMatchApplication.java
@@ -0,0 +1,13 @@
+package io.github.charlesvall.mobamatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobaMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobaMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/charlesvall/mobamatch/MobaMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.charlesvall.mobamatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobaMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
